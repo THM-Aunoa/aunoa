3d5a3cfce1142bd345106e023047c8bb0acd80eb
Marvin Seck-Eichhorn <marvin.seck-eichhorn@mni.thm.de>
2022-01-31 23:28:01 +0100

    update myRules

--- app/src/main/java/de/mseprojekt/aunoa/feature_app/presentation/util/card/Card.kt
+++ app/src/main/java/de/mseprojekt/aunoa/feature_app/presentation/util/card/Card.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
@@ -57,7 +58,7 @@ fun AunoaCard(
                     IconButton(
                         onClick = iconAction.onClick,
                         ) {
-                        Icon(iconAction.icon, iconAction.label)
+                        Icon(iconAction.icon, iconAction.label, tint = Color.Gray)
                     }
                 }
             }
